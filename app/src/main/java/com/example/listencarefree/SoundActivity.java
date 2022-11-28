bd6699a6ede0421743f58b16de8b68ffa8d09024
kimseongsoo <tjdtn1031@naver.com>
2022-11-28 18:45:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/listencarefree/SoundActivity.java
@@ -0,0 +1,14 @@
+package com.example.listencarefree;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_sound);
+    }
+}
